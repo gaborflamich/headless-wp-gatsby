c6f6479268b705b8f2f2a9b6f113f306699f5c34
Gabor Flamich <gabor.flamich@trendency.hu>
2024-12-27 16:10:37 +0100

    init

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+const IndexPage = () => {
+  return <main><h1>This is the about page</h1></main>;
+};
+
+export default IndexPage;
+
+export const Head = () => (
+    <>
+    <meta name="description" content="This is page description"></meta>
+        <title>About Page</title>
+    </>
+);
