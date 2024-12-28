c4c1f46d2356521fc74b354f13c0801b25346f7a
gabor.flamich <gabor.flamich@trendency.hu>
2024-12-28 19:42:22 +0100

    MediaText component created

---
+++ src/config/blockRendererComponents.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {
+  BlockRenderer,
+  getStyles,
+  getClasses,
+} from "@webdeveducation/wp-block-tools";
+
+import { MediaText } from "../components";
+
+export const BlockRendererComponents = (block) => {
+  switch (block.name) {
+    case "core/media-text": {
+      console.log("Render component: ", block);
+      return (
+        <MediaText
+          key={block.id}
+          className={getClasses(block)}
+          style={getStyles(block)}
+          verticalAlignment={block.attributes.verticalAlignment}
+          gatsbyImage={block.attributes.gatsbyImage}
+          mediaPosition={block.attributes.mediaPosition}
+        >
+          <BlockRenderer blocks={block.innerBlocks} />
+        </MediaText>
+      );
+    }
+    default:
+      return null;
+  }
+};
--- src/templates/page.js
+++ src/templates/page.js
@@ -1,46 +1,13 @@
 import React from "react";
-import {
-  BlockRendererProvider,
-  BlockRenderer,
-  getStyles,
-  getClasses,
-} from "@webdeveducation/wp-block-tools";
-import { GatsbyImage } from "gatsby-plugin-image";
+import { BlockRendererProvider } from "@webdeveducation/wp-block-tools";
+import { BlockRendererComponents } from "../config/blockRendererComponents";
 
 const Page = (props) => {
   console.log("Page props: ", props);
   return (
     <BlockRendererProvider
       allBlocks={props.pageContext.blocks}
-      renderComponent={(block) => {
-        switch (block.name) {
-          case "core/media-text": {
-            const content = (
-              <div className={`flex items-center p-4`}>
-                <div>
-                  <BlockRenderer blocks={block.innerBlocks} />
-                </div>
-              </div>
-            );
-            console.log("Render component: ", block);
-            return (
-              <div
-                key={block.id}
-                style={getStyles(block)}
-                className={getClasses(block)}
-              >
-                {block.attributes.mediaPosition === "right" && content}
-                <div>
-                  <GatsbyImage alt="" image={block.attributes.gatsbyImage} />
-                </div>
-                {block.attributes.mediaPosition !== "right" && content}
-              </div>
-            );
-          }
-          default:
-            return null;
-        }
-      }}
+      renderComponent={BlockRendererComponents}
     />
   );
 };
