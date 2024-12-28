436f6e91e0cd204bb60267a96d5d7aae9fa6b47f
gabor.flamich <gabor.flamich@trendency.hu>
2024-12-28 10:27:52 +0100

    gatsby-nodes with static page

--- src/templates/page.js
+++ src/templates/page.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { BlockRendererProvider } from "@webdeveducation/wp-block-tools";
 
-const Page = (props) => {
-    console.log('Props: ', props);
-    return <BlockRendererProvider allBlocks={props.pageContext.blocks} />
-}
+// const Page = (props) => {
+//   console.log("Props: ", props);
+//   return <BlockRendererProvider allBlocks={props.pageContext.blocks} />;
+// };
+
+const Page = () => {
+  return <div>This is page template</div>;
+};
 
 export default Page;
