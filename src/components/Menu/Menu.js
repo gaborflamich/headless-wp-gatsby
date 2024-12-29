d0bef1ddf77181e128fd93fd2ccb114e36e631af
gabor.flamich <gabor.flamich@trendency.hu>
2024-12-29 10:08:05 +0100

    Navigation hierarchy

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -3,12 +3,15 @@ import { useStaticQuery, graphql } from "gatsby";
 
 export const Menu = () => {
   const data = useStaticQuery(graphql`
-    query MyQuery {
+    query MainMenuQuery {
       wpMenu {
         menuItems {
           nodes {
             label
             url
+            parentId
+            id
+            cssClasses
           }
         }
       }
@@ -17,15 +20,60 @@ export const Menu = () => {
 
   const menuItems = data.wpMenu.menuItems.nodes;
 
-  return (
-    <nav>
-      <ul>
-        {menuItems.map((item, index) => (
-          <li key={index}>
-            <a href={item.url}>{item.label}</a>
+  const buildMenuTree = (items) => {
+    const menuMap = {};
+    const menuTree = [];
+
+    // Minden menüpontot objektumba rendezzük ID alapján
+    items.forEach((item) => {
+      menuMap[item.id] = { ...item, children: [] };
+    });
+
+    // Hierarchia felépítése
+    items.forEach((item) => {
+      if (item.parentId) {
+        menuMap[item.parentId].children.push(menuMap[item.id]);
+      } else {
+        menuTree.push(menuMap[item.id]);
+      }
+    });
+
+    return menuTree;
+  };
+
+  const MenuList = ({ items, level = 0 }) => {
+    // Meghatározzuk a megfelelő className-t a szint alapján
+    const ulClassName = level === 0 ? "desktop-menu-list" : `sub-menu-${level}`;
+
+    return (
+      <ul className={ulClassName}>
+        {items.map((item) => (
+          <li
+            key={item.id}
+            className={`menu-item ${
+              item.children && item.children.length > 0
+                ? "menu-item-has-children"
+                : ""
+            } ${item.cssClasses && item.cssClasses.join(" ")}`}
+          >
+            <a href={item.url} className="menu-item-link">
+              {item.label}
+            </a>
+            {item.children && item.children.length > 0 && (
+              <MenuList items={item.children} level={level + 1} />
+            )}
           </li>
         ))}
       </ul>
+    );
+  };
+
+  // Hierarchikus struktúra létrehozása
+  const menuTree = buildMenuTree(menuItems);
+
+  return (
+    <nav className="desktop-menu">
+      <MenuList items={menuTree} />
     </nav>
   );
 };
